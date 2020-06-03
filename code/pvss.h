234d9d193d60e1174ddafd9d8b0e7d99fe5ca0a2
Eva Palandjian <epalandjian(at)protonmail.com>
2020-06-03 20:20:13 +0200

    c++

--- code/pvss.h
+++ code/pvss.h
@@ -9,22 +9,28 @@ typedef struct pl_t pl_t;
 
 void prng_init(const unsigned int seed);
 
+/* allocate the memory for the publice ledger */
 pl_t *pl_alloc(const int n);
+
+/* liberate the memory of the public ledger */
 void pl_free(pl_t *pl);
+
+/* print the public ledger */
 void pl_print(pl_t *pl);
 
+/* compute a generator of the multiplicative group Zq */
 void generator(ZZ_p& g, const ZZ& q);
 
-// setup return the secretkey
-// float *setup(const int q, const double h, pl_t *pl);
+/* setup return the public ledger
+and create the public keys and secret keys for n participants */
 pl_t *setup(Vec<ZZ_p>& sk, const int n, const ZZ& q, const ZZ& p, const ZZ_p& h);
 
-// n participants, t threshold, l = n-2t-1
-// return the list of the secrets ( one secret for one participant)
+/* t threshold, l number of secrets
+add the encrypted shares in he public ledger */
 void distribution(const int l, const int t, pl_t *pl);
 
-// int lambda(const int i, const int j, const int *tab, const int len);
+/* compute the list of lambda to compute the secrets */
 void lambda(Mat<ZZ_p>& lamb, int t, pl_t *pl);
 
-// reconstruct the secret vector (well... now no... but soon...)
+/* reconstruct the secret vector */
 void reconstruction(Vec<ZZ>& S, const int r, pl_t *pl);
