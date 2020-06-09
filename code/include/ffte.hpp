31149509be7ae2fe3a566609c748ef1c1de9f82e
Eva Palandjian <epalandjian(at)protonmail.com>
2020-06-09 21:43:25 +0200

    test of efficiency

--- code/include/ffte.hpp
+++ code/include/ffte.hpp
@@ -12,3 +12,5 @@ void FFTE(Vec<ZZ_p>& f, const long n, const Vec<ZZ_p>& h, const ZZ& w, const ZZ&
 void evalu(ZZ& sum, const Vec<ZZ_p>& L, const ZZ& x, const ZZ& q);
 bool test(const Vec<ZZ_p>& W, const Vec<ZZ_p>& coef, const ZZ_p& h, const ZZ& w,
           const long n, const ZZ& q);
+
+/* time for one call to FFTE with q of size 1024 and n=1024 -> about 2s */
