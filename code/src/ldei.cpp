c24f788a7c3d962455f9c3d1caaa9045d5e18e89
evapln <epalandjian@protonmail.com>
2020-07-09 15:12:44 +0200

    add of ldei w/o interaction

--- code/src/ldei.cpp
+++ code/src/ldei.cpp
@@ -1,10 +1,153 @@
 #include <cstdbool>
 #include <NTL/ZZ_pX.h>
+
 #include "ldei.hpp"
+#include "hash.hpp"
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////// LDEI ////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+struct ldei_t {
+  Vec<ZZ_p> a;
+  ZZ_p e;
+  ZZ_pX z;
+};
+
+ldei_t *ldei_alloc(const int m) {
+  ldei_t *ld;
+  ld = new (nothrow) ldei_t;
+  if (!ld)
+    return NULL;
+  ld->a.SetLength(m);
+  ld->e = ZZ_p(0);
+  ld->z = 0;
+  return ld;
+}
+
+/* v only for test v */
+ldei_t* ldei_copy(ldei_t* src, int m) {
+  ldei_t* dest = ldei_alloc(m);
+  dest->a = src->a;
+  dest->e = src->e;
+  dest->z = src->z;
+  return dest;
+}
+
+void ldei_set_a(ldei_t* ld, Vec<ZZ_p>& a) {
+  ld->a = a;
+}
+
+void ldei_set_e(ldei_t* ld, ZZ_p& e) {
+  ld->e = e;
+}
+
+void ldei_set_z(ldei_t* ld, ZZ_pX& z) {
+  ld->z = z;
+}
+
+void ldei_free(ldei_t *ld) {
+  if (ld) {
+    ld->a.kill();
+    delete ld;
+  }
+}
+/* ^ only for test ^ */
+
+
+void ldei_print(ldei_t *ld) {
+  cout << "ldei proof:\n a = " << ld->a << "\n e = " << ld->e << "\n z = " << ld->z << endl;
+}
+
+// in ZZq (in the field)
+ldei_t* ldei_prove(const ZZ& q, const ZZ& p, const Vec<ZZ_p>& g, const Vec<ZZ_p>& alpha,
+const long k, const Vec<ZZ_p>& x, const ZZ_pX& P) {
+  ZZ_p::init(q);
+  // verify entries
+  int m = g.length();
+  if (alpha.length() != m || x.length() != m || k > m)
+    return NULL;
+
+
+  //create the vector a
+  ZZ_pX R = random_ZZ_pX(k+1);
+  Vec<ZZ_p> R_eval = eval(R, alpha);
+
+  ZZ_p::init(p);
+
+  // create ldei structure
+  ldei_t *ld = ldei_alloc(m);
+
+  for (int i = 0; i < m; i++) {
+    power(ld->a[i], g[i], rep(R_eval[i]));
+  }
+
+  // in ZZq
+  ZZ_pPush push(q);
+
+  // create e the digest of the hash function
+  hash_ZZp(ld->e, x, ld->a);
+
+  // create z the polynomial of ld
+  ZZ_pX tmp = ld->e * P;
+  ld->z = tmp + R;
+
+  return ld;
+}
+
+bool ldei_verify(const ldei_t* ld, const ZZ& q, const ZZ& p, const Vec<ZZ_p>& g,
+const Vec<ZZ_p>& alpha, const long k, const Vec<ZZ_p>& x){
+  // test of entries
+  if (!ld)
+    return false;
+  int m = ld->a.length();
+  if (alpha.length() != m || x.length() != m || g.length() != m) {
+    cout << "Verification not passed: bad length\n";
+    return false;
+  }
+
+  // test degree polynomial
+  if (deg(ld->z) > k) {
+    cout << "Verification not passed: bad degree of z\n";
+    return false;
+  }
+
+  ZZ_p::init(q);
+
+  // test of e = hash(x,a)
+  ZZ_p hashzz;
+  hash_ZZp(hashzz, x, ld->a);
+  if (ld->e != hashzz) {
+    cout << "Verification not passed: bad digest\n";
+    return false;
+  }
+
+
+  // test of x_i^e * a_i = g_i^z(alpha_i)
+  Vec<ZZ_p> zi = eval(ld->z, alpha);
+  ZZ_pPush push(p);
+  ZZ_p tmp4, tmp2, tmp3;
+  for (int i = 0; i < m; i++) {
+    power(tmp2, g[i], rep(zi[i]));
+    power(tmp3, x[i], rep(ld->e));
+    mul(tmp4, tmp3, ld->a[i]);
+    if (tmp2 != tmp4) {
+      cout << "Verification not passed: bad a_i\n";
+      return false;
+    }
+  }
+  cout << "Verification passed\n";
+  return true;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////// local LDEI ///////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 // in ZZq
 bool localldei(const Vec<ZZ_p>& a, const long k, const Vec<ZZ_p>& x,
-  const long m, const ZZ& p) {
+const long m, const ZZ& p) {
 
   // computation of vector u
   Vec<ZZ_p> u;
@@ -28,8 +171,8 @@ bool localldei(const Vec<ZZ_p>& a, const long k, const Vec<ZZ_p>& x,
   Vec<ZZ_p> v;
   v.SetLength(m);
   for (int i = 0; i < m; i++) {
-    eval(tmp,P,a[i]);
-    mul(v[i],u[i],tmp);
+    eval(tmp, P, a[i]);
+    mul(v[i], u[i], tmp);
   }
 
 
--- code/src/main.cpp
+++ code/src/main.cpp
@@ -31,6 +31,28 @@ Vec<ZZ_p> vect_rand_dist(const int n) {
   return v;
 }
 
+// in ZZp
+void generators(Vec<ZZ_p>& g, const ZZ& q, const int m) {
+  g.SetLength(m);
+  ZZ_p po;
+  int ind = 0;
+  for (int i = 2; i < 2*q+1; i++) {
+    power(po,ZZ_p(i),2);
+    if (IsOne(po))
+      continue;
+    power(po,ZZ_p(i),q);
+    if (IsOne(po))
+      continue;
+    g[ind] = ZZ_p(i);
+    ind++;
+    if (ind >= m)
+      return;
+  }
+  if (ind < m)
+    g.kill();
+  return;
+}
+
 string string_to_hex(const string& input) {
   static const char* const lut = "0123456789ABCDEF";
   size_t len = input.length();
@@ -78,8 +100,8 @@ int main(void) {
   // cout << "pvss" << endl;
   // pvss();
 
-  // test LDEI
-  // cout << "LDEI" << endl;
+  // test Local LDEI
+  // cout << "Local LDEI" << endl;
   // long m = 1024, k = 764;
   // ZZ q = GenGermainPrime_ZZ(1024);
   // ZZ p = 2 * q + 1;
@@ -89,11 +111,12 @@ int main(void) {
   // ZZ_pX P;
   // random(P,k+1);
   // Vec<ZZ_p> pi = eval(P,a);
-  // ZZ_p g;
-  // generator(g,q);
   //
   // ZZ_p::init(p);
+  // ZZ_p g;
+  // generator(g,p);
   // ZZ_p h = power(g,2);
+  // cout << g << endl << h <<  endl;
   // Vec<ZZ_p> x;
   // x.SetLength(m);
   // for (int i = 0; i < m; i++)
@@ -119,39 +142,123 @@ int main(void) {
   // cout << "It's " << verif1 << " in " << (float)time1/CLOCKS_PER_SEC << " second(s).\n";
   // cout << "It's " << verif2 << " in " << (float)time2/CLOCKS_PER_SEC << " second(s).\n";
 
-  // test file hash
-  cout << "hash" << endl;
-  long k = 102;
-  long l = 1024 - k;
-  ZZ p,q;
-  findprime(k,l,q,p);
-  cout << q << endl;
+  // test LDEI
+  cout << "LDEI" << endl;
+  long m = 1024, k = 9;
+  ZZ q = GenGermainPrime_ZZ(1024);
+  ZZ p = 2 * q + 1;
   ZZ_p::init(q);
-  ZZ_p zz_s2z, out;
-  Vec<ZZ_p> x, a;
-  x.SetLength(5);
-  for (int i = 0; i < 5; i++) x[i] = ZZ_p(i+11);
-  a.SetLength(4);
-  for (int i = 0; i < 4; i++) a[i] = ZZ_p(i+16);\
-  string s_s2z, s_z2s, in_sha, out_sha;
-  // s2z
-  cout << "string_to_ZZp" << endl;
-  s_s2z = "test";
-  string_to_ZZp(zz_s2z, s_s2z);
-  cout << "sould be " << s_s2z << " : " << zz_s2z << endl;
-  // z2s
-  cout << "ZZP_to_string" << endl;
-  s_z2s = ZZp_to_string(x, a);
-  cout << "should be " << x << a << " : " << s_z2s << endl;
-  // sha
-  cout << "sha3_512" << endl;
-  in_sha = "111213141516171819";
-  out_sha = sha3_512(in_sha);
-  cout << "hash of " << in_sha << " is " << string_to_hex(out_sha) << endl;
-  // hash_ZZp
-  cout << "hash_ZZp" << endl;
-  hash_ZZp(out, x, a);
-  cout << out << endl;
+  Vec<ZZ_p> alpha = vect_rand_dist(m);
+  ZZ_pX P = random_ZZ_pX(k+1);
+  Vec<ZZ_p> P_eval = eval(P,alpha);
+
+  ZZ_p::init(p);
+  Vec<ZZ_p> gens;
+  generators(gens, q, m);
+  if (gens.length() != m)
+    return EXIT_FAILURE;
+  Vec<ZZ_p> h;
+  h.SetLength(m);
+  for (int i = 0; i < m; i++)
+    power(h[i], gens[i], 2);
+  Vec<ZZ_p> x;
+  x.SetLength(m);
+  for (int i = 0; i < m; i++)
+    power(x[i],h[i],rep(P_eval[i]));
+
+  clock_t rec;
+  rec = clock();
+  ldei_t *ld = ldei_prove(q, p, h, alpha, k, x, P);
+  rec = clock() - rec;
+  cout << (float)rec/CLOCKS_PER_SEC << "s\n";
+  cout << "should be true : ";
+  ldei_verify(ld, q, p, h, alpha, k, x);
+
+  ldei_t* copy1 = ldei_copy(ld, m);
+  ldei_t* copy2 = ldei_copy(ld, m);
+  ldei_t* copy3 = ldei_copy(ld, m);
+  ldei_t* copy4 = ldei_copy(ld, m);
+  ldei_free(ld);
+  ZZ_p::init(q);
+
+
+  ZZ_p ef = random_ZZ_p();
+  ldei_set_e(copy1, ef);
+  cout << "should be false from hash: ";
+  rec = clock();
+  ldei_verify(copy1, q, p, h, alpha, k, x);
+  rec = clock() - rec;
+  cout << (float)rec/CLOCKS_PER_SEC << "s\n";
+  ldei_free(copy1);
+
+  ZZ_pX zf = random_ZZ_pX(k+1);
+  ldei_set_z(copy2, zf);
+  cout << "should be false from ai: ";
+  rec = clock();
+  ldei_verify(copy2, q, p, h, alpha, k, x);
+  rec = clock() - rec;
+  cout << (float)rec/CLOCKS_PER_SEC << "s\n";
+  ldei_free(copy2);
+
+  Vec<ZZ_p> af;
+  af.SetLength(m);
+  for (int i = 0 ; i < m; i++)
+    random(af[i]);
+  ldei_set_a(copy3, af);
+  cout << "should be false from hash: ";
+  rec = clock();
+  ldei_verify(copy3, q, p, h, alpha, k, x);
+  rec = clock() - rec;
+  cout << (float)rec/CLOCKS_PER_SEC << "s\n";
+  ldei_free(copy3);
+
+  af.SetLength(m+1);
+  for (int i = 0 ; i < m+1; i++)
+    random(af[i]);
+  ldei_set_a(copy4, af);
+  cout << "should be false from lengths: ";
+  rec = clock();
+  ldei_verify(copy4, q, p, h, alpha, k, x);
+  rec = clock() - rec;
+  cout << (float)rec/CLOCKS_PER_SEC << "s\n";
+  ldei_free(copy4);
+
+
+
+
+  // // test file hash
+  // cout << "hash" << endl;
+  // long k = 102;
+  // long l = 1024 - k;
+  // ZZ p,q;
+  // findprime(k,l,q,p);
+  // cout << q << endl;
+  // ZZ_p::init(q);
+  // ZZ_p zz_s2z, out;
+  // Vec<ZZ_p> x, a;
+  // x.SetLength(5);
+  // for (int i = 0; i < 5; i++) x[i] = ZZ_p(i+11);
+  // a.SetLength(4);
+  // for (int i = 0; i < 4; i++) a[i] = ZZ_p(i+16);
+  // string s_s2z, s_z2s, in_sha, out_sha;
+  // // s2z
+  // cout << "string_to_ZZp" << endl;
+  // s_s2z = "test";
+  // string_to_ZZp(zz_s2z, s_s2z);
+  // cout << "sould be " << s_s2z << " : " << zz_s2z << endl;
+  // // z2s
+  // cout << "ZZP_to_string" << endl;
+  // s_z2s = ZZp_to_string(x, a);
+  // cout << "should be " << x << a << " : " << s_z2s << endl;
+  // // sha
+  // cout << "sha3_512" << endl;
+  // in_sha = "111213141516171819";
+  // out_sha = sha3_512(in_sha);
+  // cout << "hash of " << in_sha << " is " << string_to_hex(out_sha) << endl;
+  // // hash_ZZp
+  // cout << "hash_ZZp" << endl;
+  // hash_ZZp(out, x, a);
+  // cout << out << endl;
 
   // return
   return EXIT_SUCCESS;
